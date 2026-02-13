ddce5136e232bad38aa1ef4ad0243515d1d3835c
vladpogh <poghosyanvova22@gmail.com>
2026-02-13 10:10:58 +0200

    Refactor code structure for improved maintainability

--- obj/Debug/net10.0/proekt.AssemblyInfo.cs
+++ obj/Debug/net10.0/proekt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proekt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06cb98d256b2ca17be0153d3495e0d60135139fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40cb375265da8f4d711ee5d1fee49f368b4be94d")]
 [assembly: System.Reflection.AssemblyProductAttribute("proekt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
