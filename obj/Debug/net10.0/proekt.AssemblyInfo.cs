72d1435f868aa947ef873e1ea767e3a1bfd05542
vladpogh <poghosyanvova22@gmail.com>
2026-02-24 10:21:50 +0200

    Fixed visibility

--- obj/Debug/net10.0/proekt.AssemblyInfo.cs
+++ obj/Debug/net10.0/proekt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proekt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22d49b927e271c6ad85b4455eb98f36f63d9172a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13e40738fccd17dc1e676fcad9b6db1f53621729")]
 [assembly: System.Reflection.AssemblyProductAttribute("proekt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
