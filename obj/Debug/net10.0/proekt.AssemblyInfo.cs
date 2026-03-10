21b9a5677692af243a275f4ec297aaf11b530c18
vladpogh <poghosyanvova22@gmail.com>
2026-03-10 09:07:59 +0200

    update the Home Index view.

--- obj/Debug/net10.0/proekt.AssemblyInfo.cs
+++ obj/Debug/net10.0/proekt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proekt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72d1435f868aa947ef873e1ea767e3a1bfd05542")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c193673cd58e24dba3682e33231590043ee2a873")]
 [assembly: System.Reflection.AssemblyProductAttribute("proekt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
