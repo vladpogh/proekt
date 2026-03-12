8121e079418abe71d16e3a9d31c9d9ace25b95f3
vladpogh <poghosyanvova22@gmail.com>
2026-03-12 10:51:25 +0200

    Update translation service and refine admin and doctor application detail views, alongside regenerating static web asset manifests due to new uploads and build artifacts.

--- obj/Debug/net10.0/proekt.AssemblyInfo.cs
+++ obj/Debug/net10.0/proekt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proekt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2726eb2b5a975bab31144195b69ae771401d82cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74ad9c7c730018b3d8dd094b2a4edce61d4fd9d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("proekt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
