ed1d84ee50473faeb1cc535a6c10e90d7df02dca
vladpogh <poghosyanvova22@gmail.com>
2026-03-16 14:19:29 +0200

    chore: Regenerate build artifacts and cache files.

--- obj/Debug/net10.0/proekt.AssemblyInfo.cs
+++ obj/Debug/net10.0/proekt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proekt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9178a21a588573974962000a0f7256d6ca55759")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+feb7775d8f64437a66b52c42b43645f7e8a25b12")]
 [assembly: System.Reflection.AssemblyProductAttribute("proekt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
