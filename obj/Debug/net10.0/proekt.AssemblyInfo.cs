be36786e159a0031de2f2349068e5da095c56e58
vladpogh <poghosyanvova22@gmail.com>
2026-03-17 09:04:54 +0200

    build: Recompile project and update build artifacts, including a change to the Contact view.

--- obj/Debug/net10.0/proekt.AssemblyInfo.cs
+++ obj/Debug/net10.0/proekt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proekt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed1d84ee50473faeb1cc535a6c10e90d7df02dca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9351de12b64a73322414b45fd65a954b549aa3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("proekt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
